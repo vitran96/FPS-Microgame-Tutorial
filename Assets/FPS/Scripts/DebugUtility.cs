e3dff3c7a8c25e615da97da557ecbeb2d173de1a
Chris8018 <tranuytrieuvi@protonmail.com>
2019-11-17 20:53:40 +0700

    1st Commit

---
+++ Assets/FPS/Scripts/DebugUtility.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public static class DebugUtility
+{
+    public static void HandleErrorIfNullGetComponent<TO, TS>(Component component, Component source, GameObject onObject)
+    {
+#if UNITY_EDITOR
+        if (component == null)
+        {
+            Debug.LogError("Error: Component of type " + typeof(TS) + " on GameObject " + source.gameObject.name + 
+                " expected to find a component of type " + typeof(TO) + " on GameObject " + onObject.name + ", but none were found.");
+        }
+#endif
+    }
+
+    public static void HandleErrorIfNullFindObject<TO, TS>(UnityEngine.Object obj, Component source)
+    {
+#if UNITY_EDITOR
+        if (obj == null)
+        {
+            Debug.LogError("Error: Component of type " + typeof(TS) + " on GameObject " + source.gameObject.name + 
+                " expected to find an object of type " + typeof(TO) + " in the scene, but none were found.");
+        }
+#endif
+    }
+}
